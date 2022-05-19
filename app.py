4179f0197f4cfab25d47e3e0de28e337a880d707
Kazu Takahashi <num96kazu19@icloud.com>
2022-05-19 18:49:07 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+# 実行は保存してから ctrl + s --> ctrl + alt + n
+print("Hello World")
+
+'''
+command line basic
+pwd = print working directory
+ls = lists
+cd = change directory
+mkdir = make directory
+cd .. = 親ディレクトリに移動する
+clear = terminalを綺麗にする
+'''
